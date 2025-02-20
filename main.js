d6753b112caf3667d9b480c08ca3b14ce4ed58de
Ayham <ayhamhassouneh@gmail.com>
2025-02-20 03:37:49 +0300

    Done

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ for(let i=0; i<numberOfColorBoxes; i++){
     colorBox.classList.add('colorBox');
     bgAnimation.append(colorBox)
 }
+
+
+
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHieght,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.header__menu a[herf*=' + sectionId + ']')
+            
+    })
+}
+window.addEventListener('scroll', scrollActive)
